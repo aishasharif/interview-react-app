c1d5a9679fb283f0d4506aac462e3508495c92a2
Aisha Ali <aishaali@Aishas-MBP.Home>
2025-03-29 22:34:06 +0000

    test

--- my-app/infra/backend.tf
+++ my-app/infra/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "tf-backend-rg"
     storage_account_name = "tfstatestorage"
     container_name       = "tfstate"
-    key                  = ""
+    key                  = "react-app/terraform.tfstate"
   }
 }
