5851ad937f722a75e46945532e20cdbb2eab1300
Aisha Ali <aishaali@Aishas-MBP.Home>
2025-03-30 03:07:56 +0100

    update

--- my-app/infra/modules/app/app.tf
+++ my-app/infra/modules/app/app.tf
@@ -3,6 +3,11 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+resource "time_sleep" "wait_for_rg" {
+  depends_on = [azurerm_resource_group.rg]
+  create_duration = "20s"
+}
+
 resource "azurerm_static_web_app" "webapp" {
   name                = "webapp-${terraform.workspace}"
   resource_group_name = azurerm_resource_group.rg.name
