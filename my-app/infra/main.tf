ebdae385c871b5ea7bc56bae9d656b7a6e10f316
Aisha Ali <aishaali@Aishas-MBP.Home>
2025-03-30 02:43:40 +0100

    update version

--- my-app/infra/main.tf
+++ my-app/infra/main.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.0"
+      version = "=4.24.0"
     }
   }
-  required_version = ">= 0.14.9"
-  
+
 }
 provider "azurerm" {
   features {}
