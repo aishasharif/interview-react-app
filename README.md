5674e5a1c1f35e0c5273cb25917a7a899c5f46f9
Aisha Ali <aishaali@Aishas-MBP.Home>
2025-03-30 02:33:23 +0100

    update

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # interview-react-app
 
 
+az storage account create \
+  --name tfstatestorage \
+  --resource-group tf-backend-rg \
+  --location uksouth \
+  --sku Standard_LRS \
+  --encryption-services blob
+
+
+  az account set --subscription b5544d16-7b6b-4e42-a36a-d95ef103e0d9
--- my-app/infra/backend.tf
+++ my-app/infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tf-backend-rg"
-    storage_account_name = "tfstatestorage"
+    storage_account_name = "tfstatestorage3004"
     container_name       = "tfstate"
     key                  = "react-app/terraform.tfstate"
   }
